f641d6fd84668bd3415a1f88574864497301c4e5
Gentaro Miura <si100028@edu.aomori-u.ac.jp>
2014-01-24 11:03:54 +0900

    アプリケーション初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShelterInfo::Application.config.secret_token = '10c859fac3b7d6e78cbc7fa42d41023ed3eddcf454d6fb68b3959fadc31dde3f5fc4b460d07ca298462780a5dde02ccaf03cc6ed1342b7d2fd747e744d1bc1dd'
